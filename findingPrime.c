db229f1568995c6e8f3c17df69b1ffc9c3b144c9
Esma Bektas <116214967+esmbkts@users.noreply.github.com>
2024-08-22 15:37:19 +0300

    Add files via upload

---
+++ findingPrime.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int number, num;
+
+	printf("Enter an integer: ");
+	scanf("%d", &number);
+
+	for (num = 2; num < number; num++)
+		if (number % num == 0)
+			break;
+
+	if (num == number)
+		printf("PRIME!");
+	else
+		printf("NOT prime!");
+
+	return 0;
+}
